072f1e6ad8e6f2f13b67cc9833842669374cd45a
bdcf <53155385+bdcf@users.noreply.github.com>
2025-11-03 10:40:28 -0500

    Create Binary_Search.c
    This is my binary search implementation that does cover the special case of the target not existing. This is the algorithm on which I will base my other Binary Search implementations.

---
+++ Binary_Search_Questions/Binary_Search.c
@@ -0,0 +1,15 @@
+int binarySearch(int A[], int t, int l, int r){
+    if (l > r){
+        return -1;
+    }
+    int m = floor((l + r)/2);
+    if(A[m] == t){
+        return m;
+    }
+    else if(A[m] > t){
+        return binarySearch(A, t, l, m-1);
+    }
+    else{
+        return binarySearch(A, t, m+1, r);
+    }
+}
